f7f5e26a5a45f9bc70f91c94d952001b1db18dd4
Gavin Mach <gavinmach@gmail.com>
2022-10-30 07:37:27 +1100

    Stuiff

--- src/config/store.js
+++ src/config/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import productsReducer from './features/products/productsSlice'
-import filtersReducer from './features/filters/filtersSlice'
-import cartItemsReducer from './features/cart/cartSlice'
+import productsReducer from '../features/products/productsSlice'
+import filtersReducer from '../features/filters/filtersSlice'
+import cartItemsReducer from '../features/cart/cartSlice'
 
 const store = configureStore({
   reducer: {
